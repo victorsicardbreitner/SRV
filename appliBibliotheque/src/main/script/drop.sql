4c33234a599acb6bdf60cf13692f413944d52512
Roland Panzou <panzouroland@gmail.com>
2023-07-27 16:16:40 +0200

    Init Base admin

--- appliBibliotheque/src/main/script/drop.sql
+++ appliBibliotheque/src/main/script/drop.sql
@@ -727,3 +727,5 @@ drop table livre cascade constraints;
 drop table personne cascade constraints;
 drop table livre cascade constraints;
 drop table personne cascade constraints;
+drop table livre cascade constraints;
+drop table personne cascade constraints;
