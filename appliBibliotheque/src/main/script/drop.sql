68e5ea0fb16e2f698829287477c455590fe21bd7
Roland Panzou <panzouroland@gmail.com>
2023-07-27 16:14:20 +0200

    drop create

--- appliBibliotheque/src/main/script/drop.sql
+++ appliBibliotheque/src/main/script/drop.sql
@@ -725,3 +725,5 @@ drop table livre cascade constraints;
 drop table personne cascade constraints;
 drop table livre cascade constraints;
 drop table personne cascade constraints;
+drop table livre cascade constraints;
+drop table personne cascade constraints;
