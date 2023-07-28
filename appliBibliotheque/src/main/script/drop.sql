40cd6f9b8e2b608f5d54f3a61f4a65b157a659d7
victorsicardbreitner <81385307+victorsicardbreitner@users.noreply.github.com>
2023-07-28 13:31:32 +0200

    pull dev

--- appliBibliotheque/src/main/script/drop.sql
+++ appliBibliotheque/src/main/script/drop.sql
@@ -1046,3 +1046,6 @@ drop table personne cascade constraints;
 drop table emprunt cascade constraints;
 drop table livre cascade constraints;
 drop table personne cascade constraints;
+drop table emprunt cascade constraints;
+drop table livre cascade constraints;
+drop table personne cascade constraints;
