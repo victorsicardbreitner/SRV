c9f093afe00888d018a6e8db96982356e87a6fe1
victorsicardbreitner <81385307+victorsicardbreitner@users.noreply.github.com>
2023-07-28 13:34:26 +0200

    modif gitignore

--- appliBibliotheque/src/main/script/drop.sql
+++ appliBibliotheque/src/main/script/drop.sql
@@ -1070,3 +1070,6 @@ drop table personne cascade constraints;
 drop table emprunt cascade constraints;
 drop table livre cascade constraints;
 drop table personne cascade constraints;
+drop table emprunt cascade constraints;
+drop table livre cascade constraints;
+drop table personne cascade constraints;
